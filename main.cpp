275508cfa9596ac953f10675172343278948210f
Marek <marqus34@gmail.com>
2018-03-04 20:01:01 +0100

    Input file setup

--- main.cpp
+++ main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
+#include <fstream>
 
-int main(){
-    std::cout<<"Hello world!"<<std::endl;
+using namespace std;
+
+int main()
+{
+    string filenameIn = "input.txt";
+
+    ifstream fin;
+    fin.open(filenameIn, ifstream::in);
+
+    int num;
+    fin >> num;
+    cout << num << endl;
 
     return 0;
 }
