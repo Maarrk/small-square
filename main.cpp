e37ee28baff5bd7f9571566a0099b98a2a411887
Marek <marqus34@gmail.com>
2018-03-04 21:24:41 +0100

    Imported code from VS project

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 #include <fstream>
+#include <float.h>
 
 using namespace std;
+
+double critForce(double x, double a, double b);
+void critParams(double x1, double y1, double x2, double y2, double& a, double& b);
 
 int main()
 {
@@ -10,9 +14,69 @@ int main()
     ifstream fin;
     fin.open(filenameIn, ifstream::in);
 
-    int num;
-    fin >> num;
-    cout << num << endl;
+    if (!fin.is_open())
+    {
+        cout << "Could not open \"" + filenameIn + "\"" << endl;
+        system("pause");
+        return 0;
+    }
+
+    int pointCount;
+    fin >> pointCount;
+
+    double* px = (double*)malloc(pointCount * sizeof(double)); // points X coordinate
+    double* py = (double*)malloc(pointCount * sizeof(double)); // points Y coordinate
+
+    for (int i = 0; i < pointCount; i++)
+    {
+        fin >> px[i] >> py[i];
+    }
+
+    // Find average function params
+
+    double a, b;
+    double aSum = 0.0, bSum = 0.0;
+
+    for (int i = 0; i < pointCount; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            critParams(px[i], py[i], px[j], py[j], a, b);
+            aSum += a;
+            bSum += b;
+        }
+    }
+
+    a = aSum / pointCount;
+    b = bSum / pointCount;
+
+    auto f = [a, b](double x) { return critForce(x, a, b); }; // Final function
+
+    cout << "i:\tx:\ty:\tf(x):" << endl;
+    for (int i = 0; i < pointCount; i++)
+    {
+        cout << i << ":\t" << px[i] << "\t" << py[i] << "\t" << f(px[i]) << endl;
+    }
+
+    system("pause");
+
+    free(py);
+    free(px);
 
     return 0;
+}
+
+///<summary>Hyperbolic function with asymptotes: y=0; x=b</summary>
+double critForce(double x, double a, double b)
+{
+    if (x == b) return DBL_MAX; // Prevent 0 division, make cost function high
+
+    return a / (x - b);
+}
+
+///<summary>Returns parameters for critForce given two points</summary>
+void critParams(double x1, double y1, double x2, double y2, double & a, double & b)
+{
+    a = (y1*y2*(x2 - x1)) / (y1 - y2);
+    b = (y1*x1 - y2 * x2) / (y1 - y2);
 }
